3ed53fa950962d3e462c0d2da0a1d645585a1e5b
Jacob Michelsen <chromegadget@gmail.com>
2016-04-06 20:25:52 +0200

    Starting to add edit view

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// using an ES6 transpiler, like babel
+import { Router, Route, Link } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ControlBox from './components/controlbox';
+import EditDrawings from './components/editdrawings';
+
+
+  ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Controlbox}>
+      <Route path="edit" component={EditDrawings}/>
+    </Route>
+  </Router>
+  ), document.getElementById('root'));
